a1f54cce18c5520b99a65320761ced29995fa049
LBaldassarre <lbaldassarre.dev@gmail.com>
2024-07-04 10:13:05 +0200

    Creat User Post

--- controller/usersCT.js
+++ controller/usersCT.js
@@ -2,16 +2,15 @@ import { User } from "../model/usersM.js";
 
 //register new user
 export const createUser = (req, res, next) => {
-  const { fullName, userName, email, password } = req.body;
-  const newUser = new User({ fullName, userName, email, password });
+  const { userName, email, password } = req.body;
+  const character_id = [];
+  const newUser = new User({ userName, email, password, character_id });
   newUser
     .save()
     .then((info) => {
-      console.log(info);
       res.status(200).json({ message: "New user successfully created." });
     })
     .catch((err) => {
-      console.log(err);
       return next(err);
     });
 };
