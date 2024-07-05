a4e7a5dec135aa2c1fbd5f2811b743fb2b933727
LBaldassarre <lbaldassarre.dev@gmail.com>
2024-07-05 09:52:22 +0200

    Add favorites controller

--- controller/usersCT.js
+++ controller/usersCT.js
@@ -19,3 +19,30 @@ export const getAllUsers = async (req, res, next) => {
   const users = await User.find({});
   res.status(200).json({ users });
 };
+
+export const logIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  const [user] = await User.find({ email: email });
+  if (user) {
+    if (password === user.password) {
+      res.status(200).json({ mensaje: "Log In Successful", data: user })
+    } else {
+      res.status(400).json({ mensaje: "Wrong Password" })
+    }
+  } else {
+    res.status(500).json({ mensaje: "Email not found" })
+  }
+};
+
+export const addFavorite = async (req, res, next) => {
+  const { email, characters_id } = req.body;
+  const update = await User.updateOne(
+    { email: email },
+    {
+      $set: { characters_id: characters_id }
+    }
+  );
+  update
+    ? res.status(200).json({ mensaje: "Character List Updated" })
+    : res.status(500).json({ mensaje: "Internal Server Error" })
+};
--- routes/usersRT.js
+++ routes/usersRT.js
@@ -1,5 +1,7 @@
-import { createUser, getAllUsers} from "../controller/usersCT.js";
+import { createUser, getAllUsers, logIn, addFavorite} from "../controller/usersCT.js";
 import { Router } from "express";
 export const router = Router();
 router.post("/", createUser);
 router.get("/", getAllUsers);
+router.post("/login", logIn);
+router.patch("/characters", addFavorite);
