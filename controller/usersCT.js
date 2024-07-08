59f0d7e23c88a3f7558a45ab5e575975bf683418
LBaldassarre <lbaldassarre.dev@gmail.com>
2024-07-08 16:25:18 +0200

    Add validator for Sing Up

--- controller/usersCT.js
+++ controller/usersCT.js
@@ -1,28 +1,42 @@
 import { User } from "../model/usersM.js";
+import { validateUser } from "../validator/userSchema.js";
 
 //register new user
-export const createUser = (req, res, next) => {
+export const createUser = async (req, res, next) => {
   const { userName, email, password } = req.body;
   const characters_id = "[]";
-  const newUser = new User({ userName, email, password, characters_id });
-  newUser
-    .save()
-    .then((info) => {
-      res.status(200).json({ message: "New user successfully created." });
-    })
-    .catch((err) => {
-      return next(err);
-    });
+  const validatedUser = validateUser({
+    userName,
+    email,
+    password,
+    characters_id
+  });
+  if (validatedUser.error) {
+    const [path] = validatedUser.error.issues[0].path;
+    const error = validatedUser.error.issues[0].message;
+    const message = path + ': ' + error
+    res.status(422).json({message: message});
+  } else {
+    const newUser = new User({ userName, email, password, characters_id });
+    newUser
+      .save()
+      .then((info) => {
+        res.status(200).json({ message: "New user successfully created" });
+      })
+      .catch((err) => {
+        return next(err);
+      });
+  }
 };
 
 export const getAllUsers = async (req, res, next) => {
   const users = await User.find({});
-  res.status(200).json({data: users });
+  res.status(200).json({ data: users });
 };
 
 export const getUser = async (req, res, next) => {
   const { email } = req.params;
-  const [ user ] = await User.find({ email: email });
+  const [user] = await User.find({ email: email });
   res.status(200).json({ user });
 };
 
